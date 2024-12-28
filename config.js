30b42902e1bf5f294e0250c7b0dc0e5a78a270d7
Olamilekanb450 <partyhalloween59@gmail.com>
2024-12-28 22:44:03 +0100

    Update config.js

--- config.js
+++ config.js
@@ -11,9 +11,9 @@ module.exports = {
   ANTILINK: toBool(process.env.ANTI_LINK) || false,
   LOGS: toBool(process.env.LOGS) || false,
   ANTILINK_ACTION: process.env.ANTI_LINK || "kick",
-  SESSION_ID:process.env.SESSION_ID || "Nikka-XJ2gB0L5B#3EIckMQg73EIjh7Xb_sO24PUPDtwZyucWzFqSpt4S9M",
+  SESSION_ID:process.env.SESSION_ID || "Nikka-XE2IlxQJL#6mh8W-TK1Td6iaw_ZHjTDXol0YEmMCJtmmRFd1k2auQNikka-XE2IlxQJL#6mh8W-TK1Td6iaw_ZHjTDXol0YEmMCJtmmRFd1k2auQ",
   LANG: process.env.LANG || "EN",
-  HANDLERS: process.env.PREFIX || '^[!]',
+  HANDLERS: process.env.PREFIX || '^[?]',
   PRESCENCE: process.env.PRESCENCE || "typing",
   BRANCH: "main",
   WARN_COUNT: 3,
@@ -44,6 +44,6 @@ module.exports = {
         }),
   HEROKU_APP_NAME: process.env.HEROKU_APP_NAME || " ",
   HEROKU_API_KEY: process.env.HEROKU_API_KEY || " ",
-  SUDO: process.env.SUDO || "2349112171078,94703981512", // replace ur number
+  SUDO: process.env.SUDO || "2349073315641,2349073315641", // replace ur number
   IMGBB_KEY: ["76a050f031972d9f27e329d767dd988f", "deb80cd12ababea1c9b9a8ad6ce3fab2", "78c84c62b32a88e86daf87dd509a657a"],
 };
