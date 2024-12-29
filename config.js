1353829df14c6d63092e08450de195877eb8d553
Olamilekanb450 <partyhalloween59@gmail.com>
2024-12-29 18:12:42 +0100

    Update config.js

--- config.js
+++ config.js
@@ -11,7 +11,7 @@ module.exports = {
   ANTILINK: toBool(process.env.ANTI_LINK) || false,
   LOGS: toBool(process.env.LOGS) || false,
   ANTILINK_ACTION: process.env.ANTI_LINK || "kick",
-  SESSION_ID:process.env.SESSION_ID || "Nikka-XE2IlxQJL#6mh8W-TK1Td6iaw_ZHjTDXol0YEmMCJtmmRFd1k2auQNikka-XE2IlxQJL#6mh8W-TK1Td6iaw_ZHjTDXol0YEmMCJtmmRFd1k2auQ",
+  SESSION_ID:process.env.SESSION_ID || "Nikka-XE2IlxQJL#6mh8W-TK1Td6iaw_ZHjTDXol0YEmMCJtmmRFd1k2auQ",
   LANG: process.env.LANG || "EN",
   HANDLERS: process.env.PREFIX || '^[?]',
   PRESCENCE: process.env.PRESCENCE || "typing",
